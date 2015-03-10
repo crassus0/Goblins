9eb377af1f62d72483ef6412c83b7d3a2a68c42d
Volodymyr Tsaruk <hirurg123@gmail.com>
2015-03-10 17:37:04 +0200

    Constatnts file added

---
+++ Assets/Constants.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants : MonoBehaviour {
+    public static int RaycastMaskPhysics;
+	void Awake () 
+    {
+	    RaycastMaskPhysics = LayerMask.GetMask("Physics Objects");
+	}
+}
---
+++ Assets/Mobs/MineCourier/MineCourier.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineCourier : CustomMob 
+{
+    public GameObject Target;
+    public int BearedPrice = 100;
+
+    public override int DestructionPrice
+    {
+        get { return m_destructionPrice; }
+    }
+    int m_destructionPrice = 0;
+    protected override void Start()
+    {
+    }
+
+    protected override void FixedUpdate()
+    {
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject==Target)
+        {
+            m_destructionPrice = BearedPrice;
+            Destroy(gameObject);
+        }
+
+    }
+
+    protected override void OnCollisionExit2D(Collision2D collision)
+    {
+    }
+}
