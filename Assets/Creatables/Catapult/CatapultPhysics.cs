a158b0d8c47e1d6abe9b8118e06b835a332fc56b
Volodymyr Tsaruk <hirurg123@gmail.com>
2015-04-07 16:11:43 +0300

    Basic catapult data added

---
+++ Assets/Creatables/Catapult/CatapultPhysics.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatapultPhysics : CreatableObject
+{
+    public override string IconName
+    {
+        get { throw new System.NotImplementedException(); }
+    }
+
+    public override int Price
+    {
+        get { return 100; }
+    }
+
+    public override int DestructionPrice
+    {
+        get { return 0; }
+    }
+
+    protected override void FixedUpdate()
+    {
+
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+
+    protected override void OnCollisionExit2D(Collision2D collision)
+    {
+    }
+
+    protected override void Start()
+    {
+    }
+}
---
+++ Assets/Creatables/Catapult/CatapultVisualizer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatapultVisualizer : MonoBehaviour 
+{
+    public GameObject Shell;
+    public GameObject EmptyShell;
+    public Transform ShellTransform;
+
+    public void DisconnectShell()
+    {
+        Shell.transform.parent = null;
+    }
+}
